16a3521edd6014b15e72eeca2c47fd6f637a5394
Beylul Kbreab <bkbreab8@gmail.com>
2019-04-03 14:18:35 -0700

    Add star work search bar and display

---
+++ src/components/star-war-index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import  SearchBar  from "./search-bar"
+import  StarWarList from './star-war-list'
+import swapi  from '../../apis/swapi'
+
+export default class StarWarIndex extends Component {
+
+  state = { person: '' }
+  componentDidMount() {
+    this.onFormSubmit('1')
+  }
+  onFormSubmit = async term => {
+    const response = await swapi.get(`${term}/`)
+    this.setState({person: response.data})
+  }
+
+  render() {
+  return (
+    <div>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <SearchBar onFormSubmit={this.onFormSubmit} />
+    </div>
+    <StarWarList person={this.state.person} />
+    </div>
+  )
+  }
+}
